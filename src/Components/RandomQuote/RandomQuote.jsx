0741dc020ddd14895b275cf72a55d06361b58d9f
Ngoc <ngoc.held9@gmail.com>
2024-08-05 17:11:50 +0900

    quotes state

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './RandomQuote.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import reload_icon from '../Assets/reload-icon.png'
 import twitter_icon from '../Assets/twitter-icon.png'
 
-
 export const RandomQuote = () => {
-
-  let quotes = [];
-
-  async function loadQuotes(){
-    const response = await fetch('https://type.fit/api/quotes');
-    quotes = await response.json();
-    console.log(quotes);
-  }
-
-  const[quote,setQuote] = useState({
+  const [quotes, setQuotes] = useState([]); // State to store quotes
+  const [quote, setQuote] = useState({
     text: 'Difficulties increase the nearer we get to the goal.',
     author: 'Johann Wolfgang von Goethe'
-  })
+  });
 
+  // Function to fetch quotes
+  async function loadQuotes() {
+    const response = await fetch('https://type.fit/api/quotes');
+    const data = await response.json();
+    setQuotes(data); // Update state with fetched quotes
+  }
 
-  loadQuotes();
+  // Function to set a random quote
+  const getRandomQuote = () => {
+    if (quotes.length > 0) {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      setQuote(quotes[randomIndex]);
+    }
+  }
+
+  // Fetch quotes once when component mounts
+  useEffect(() => {
+    loadQuotes();
+  }, []);
 
   return (
     <div className='container'>
@@ -29,10 +37,25 @@ export const RandomQuote = () => {
       <div>
         <div className="line"></div>
         <div className="bottom">
-          <div className="author">{quote.author}</div>
+          <div className="author">- {quote.author.split(',')[0]}</div>
           <div className="icons">
-            <img src={reload_icon} alt="" />
-            <img src={twitter_icon} alt="" />
+            <img 
+              onClick={getRandomQuote} 
+              src={reload_icon} 
+              alt="Reload" 
+              style={{ cursor: 'pointer' }} 
+            />
+            <a 
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote.text}" - ${quote.author}`)}`} 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <img 
+                src={twitter_icon} 
+                alt="Tweet" 
+                style={{ cursor: 'pointer' }} 
+              />
+            </a>
           </div>
         </div>
       </div>
